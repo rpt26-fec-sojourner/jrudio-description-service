f7e12a64221be2889cfccd621ce07c69b16da017
jrudio <justinjrudio@gmail.com>
2021-03-14 23:05:39 -0700

    integration test progress

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch');
 const db = require('./db');
-const seedDB = require('../server/database/seed');
+const { seedDatabase } = require('../server/database/seed');
 
 let dbConnection = null;
-const host = 'http:/localhost';
+const host = 'http://localhost';
 const port = 7878;
 const baseURL = `${host}:${port}`;
 
@@ -20,11 +20,13 @@ const baseURL = `${host}:${port}`;
 
 beforeAll(() => {
   return db()
-    .then(port => {
+    .then(connInfo => {
+      const { port } = connInfo;
+
       // set port before connecting to db
       process.env.MONGO_PORT = port;
 
-      return seedDB();
+      return seedDatabase();
     }).then(() => {
       // start app server
       require('../server');
@@ -32,8 +34,9 @@ beforeAll(() => {
     .catch(err => console.log(`could not set up db or app server: ${err}`));
 });
 
+console.log(baseURL);
 describe('routes should return the correct data from the database', () => {
-  test('should return a full listing data from /listin/:id', () => {
+  test('should return a full listing data from /listing/:id', () => {
     return fetch(`${baseURL}/listing/1`)
       .then(resp => {
         console.log(resp.status);
