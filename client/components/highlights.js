7dc0a18ef0df7d02366101623492c5bfd0846153
jrudio <justinjrudio@gmail.com>
2021-04-09 18:45:47 -0700

    listing highlights have proper font size for large screens

--- client/components/highlights.js
+++ client/components/highlights.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import styles from '../styles/highlights.module.css';
+import appStyle from '../styles/app.module.css';
 
 const ListingHighlights = (props) => {
   const highlights = props.highlights ? props.highlights.map((highlight, i) => {
-    return (<p key={i}>{highlight.title} - {highlight.subtitle}</p>);
+    return (
+      <div className={appStyle.text} key={i}>
+        <div className={styles.title}>{highlight.title}</div>
+        <div className={styles.subtitle}>{highlight.subtitle}</div>
+      </div>
+    );
   }) : null;
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       {highlights}
     </div>
   );
