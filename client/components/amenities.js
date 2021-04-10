472e1f058e2c79673398b8267e81e93f08dce9e8
jrudio <justinjrudio@gmail.com>
2021-04-10 12:18:46 -0700

    basic amenity structure

--- client/components/amenities.js
+++ client/components/amenities.js
@@ -7,22 +7,88 @@ import {
   combineStyles,
 } from '../helpers';
 
+const amenitiesWithIcons = new Set([
+  'dedicated workspace',
+  'tv',
+  'heating',
+  'air conditioning',
+  'washer',
+  'dryer',
+  'wifi',
+  'kitchen'
+]);
+
 const Amenity = (props) => {
-  return null;
+  const {
+    name
+  } = props;
+
+  return (
+    <div className={styles.amenityWrapper2}>
+      <div className={styles.amenityWrapper3}>
+        <div className={styles.name}>{name}</div>
+
+        <div className={styles.iconWrapper}>
+          <Icon name={name} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const renderAmenities = (amenities = []) => {
+  const amenityNodes = [];
+  const whitelist = new Set();
+  let count = 0;
+
+  for (let i = 0; i < amenities.length; i++) {
+    let subAmenities = amenities[i];
+
+
+    for (let j = 0; j < subAmenities.amenity.length; j++) {
+      let amenity = subAmenities.amenity[j];
+
+      count++;
+
+      if (!amenity.isAvailable || !amenitiesWithIcons.has(amenity.name) || whitelist.has(amenity.name) ) {
+        continue;
+      }
+
+      whitelist.add(amenity.name);
+
+      amenityNodes.push(
+        <Amenity key={amenity._id} name={amenity.name} />
+      );
+    }
+  }
+
+  return {
+    count,
+    nodes: amenityNodes
+  };
 };
 
 const Amenities = (props) => {
-  const amenities = props.amenities ? props.amenities.map((amenities, i) => {
-    return amenities.amenity.map((amenity, i) => {
-      let _amenity = amenity.name;
+  const handleShowAll = (event) => {
+    event.preventDefault();
 
-      if (!amenity.isAvailable) {
-        _amenity = <s>{amenity.name}</s>;
-      }
+    console.log('showing all amenities');
+  };
 
-      return (<p key={i}>{amenities.title} - {_amenity}</p>);
-    });
-  }) : null;
+  const { amenities } = props;
+  let amenityCount = 0;
+  let amenityNodes = [];
+
+  if (amenities && amenities.length > 0) {
+    const result = renderAmenities(amenities);
+
+    amenityNodes = result.nodes;
+    amenityCount = result.count;
+  }
+
+  // Airbnb only lists 8 of the amenities and then provides
+  // a button to display all
+  // When you display all of the amenities they do not have an icon
 
   const sectionStyles = combineStyles(
     // appStyles.sectionWrapper,
@@ -36,7 +102,14 @@ const Amenities = (props) => {
           <h2 className={appStyles.sectionTitle}>Amenities</h2>
         </div>
       </div>
-      {amenities}
+
+      <div className={styles.amenityWrapper}>
+        {amenityNodes}
+      </div>
+
+      <div className={styles.showAllWrapper}>
+        <a href="?showMore=1" onClick={handleShowAll}>show all {amenityCount} amenities</a>
+      </div>
     </div>
   );
 };
